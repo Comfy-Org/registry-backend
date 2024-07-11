af697a5168d8ed7f35999291bc2bb07818d40ba0
Robin Huang <robin.j.huang@gmail.com>
2024-07-11 14:12:27 -0700

    Set null values to default before alter table.

--- ent/migrate/migrations/20240710235003_migration.sql
+++ ent/migrate/migrations/20240710235003_migration.sql
@@ -1,3 +1,6 @@
+UPDATE "ci_workflow_results"
+SET "status" = 'STARTED'
+WHERE "status" IS NULL;
 -- Modify "ci_workflow_results" table
 ALTER TABLE "ci_workflow_results" ALTER COLUMN "status" TYPE character varying, ALTER COLUMN "status" SET NOT NULL, ALTER COLUMN "status" SET DEFAULT 'STARTED', ADD COLUMN "python_version" text NULL, ADD COLUMN "vram" bigint NULL, ADD COLUMN "job_trigger_user" text NULL;
 -- Modify "git_commits" table
